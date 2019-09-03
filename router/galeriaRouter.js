d962b51fe90024788f7f32fee3da0cb252c869f0
Elisandro Mello Correa <elisandro@MacBook-Air-de-Elisandro.local>
2019-09-02 22:01:32 -0300

    Implementando método GetId para recuperar dados de uma imagem especifica

--- router/galeriaRouter.js
+++ router/galeriaRouter.js
@@ -18,4 +18,19 @@ router.get("/", function(req, resp, next){
     });
 });
 
+router.get("/:id?", function(req, resp, next){
+    GaleriaModel.getId(req.params.id, function(erro, retorno){
+        let resposta = new RespostaClass();
+        if(erro){
+            resposta.erro   = true;
+            resposta.msg    = "Ocorreu um Erro";
+        } 
+        else {
+            resposta.dados = retorno;
+        }
+
+        resp.json(resposta); //converte o objeto de retorno em json
+    });
+});
+
 module.exports = router;
